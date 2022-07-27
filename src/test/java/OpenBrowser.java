485b3e7e25327adaa7540d2d3a40ac0d503c5117
nadaS90 <engnada_45@outlook.com>
2022-07-27 17:19:04 +0200

    How to use locators

--- src/test/java/OpenBrowser.java
+++ src/test/java/OpenBrowser.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,10 +15,46 @@ public class OpenBrowser {
         WebDriver driver = new ChromeDriver();
 
         //3- Navigate
-        driver.navigate().to("https://github.com/login");
+        driver.navigate().to("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
 
-        //4- close Browser
-        driver.close();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //navigate to page
+        driver.findElement(By.linkText("Form Authentication")).click();
+
+        // find text field
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+
+        // another way to click enter
+        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+
+        //click btn
+       // driver.findElement(By.className("radius")).click();
+        //driver.findElement(By.linkText("Elemental Selenium")).click();
+       // driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        //driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Get success message
+        String successMsg = driver.findElement(By.id("flash")).getText();
+        System.out.println(successMsg);
+
+
+
+        // close Browser
+        //driver.close(); -> close one tab
+        driver.quit();
 
 
 
