c10cec1e7cec37e03f8d7c8e7d59ba58ccb239c0
nadaS90 <engnada_45@outlook.com>
2022-07-29 20:20:57 +0200

    How to use TestNG annotations

--- src/test/java/OpenBrowser.java
+++ src/test/java/OpenBrowser.java
@@ -2,61 +2,52 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class OpenBrowser {
+    WebDriver driver = null;
 
-    public static void main(String[] args) {
-
-        //1- link browser with IDE
+    @BeforeTest
+    public void OpenBrowser() throws InterruptedException {
         String chromePath = System.getProperty("user.dir") + "\\src\\browsers\\chromedriver.exe";
+        System.out.println(chromePath);
         System.setProperty("webdriver.chrome.driver",chromePath);
 
-        //2- New object
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
-        //3- Navigate
-        driver.navigate().to("https://the-internet.herokuapp.com/");
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
         driver.manage().window().maximize();
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        //navigate to page
-        driver.findElement(By.linkText("Form Authentication")).click();
-
-        // find text field
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
-
-        // another way to click enter
-        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
-
-        //click btn
-       // driver.findElement(By.className("radius")).click();
-        //driver.findElement(By.linkText("Elemental Selenium")).click();
-       // driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-        //driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        //Get success message
-        String successMsg = driver.findElement(By.id("flash")).getText();
-        System.out.println(successMsg);
-
-
-
-        // close Browser
-        //driver.close(); -> close one tab
-        driver.quit();
-
-
+        Thread.sleep(3000);
 
     }
+
+    @Test
+    public void validData(){
+      //  driver.findElement(By.linkText("Form Authentication")).click();
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+
+    }
+
+   @Test
+    public void invalidData(){
+     //  driver.findElement(By.linkText("Form Authentication")).click();
+       driver.navigate().to("https://the-internet.herokuapp.com/login");
+       driver.findElement(By.id("username")).clear();
+       driver.findElement(By.id("username")).sendKeys("invalid");
+       driver.findElement(By.name("password")).sendKeys("Super");
+       driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+
+   }
+
+    @AfterTest
+    public  void closeBrowser ()throws  InterruptedException{
+        //driver.close(); -> close one tab
+        driver.quit();
+    }
+
 }
