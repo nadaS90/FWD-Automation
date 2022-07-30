ed05840d140a46e899d246641604dfd69b19a917
nadaS90 <engnada_45@outlook.com>
2022-07-30 19:36:26 +0200

    How to use Assertions

--- src/test/java/OpenBrowser.java
+++ src/test/java/OpenBrowser.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -24,12 +25,34 @@ public class OpenBrowser {
     }
 
     @Test
-    public void validData(){
+    public void validData() throws InterruptedException {
       //  driver.findElement(By.linkText("Form Authentication")).click();
         driver.findElement(By.id("username")).clear();
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+
+        //Waiting time
+        Thread.sleep(3000);
+
+        // First Assertion
+        System.out.println("first Assertion");
+        String expectedResult = "You logged into a secure area!";
+        String actualResult = driver.findElement(By.id("flash")).getText();
+        System.out.println("actual result: " + actualResult);
+        Assert.assertEquals(actualResult.contains(expectedResult), true);
+        Assert.assertTrue(actualResult.contains(expectedResult));
+
+        //Second Assertion
+        System.out.println("second Assertion");
+        Assert.assertTrue(driver.findElement(By.cssSelector("a[href=\"/logout\"]")).isDisplayed());
+
+        //Third Assertion for URL
+        System.out.println("third Assertion");
+        String actualdURL = "https://the-internet.herokuapp.com/secure";
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getCurrentUrl(),actualdURL);
+
 
     }
 
@@ -41,6 +64,11 @@ public class OpenBrowser {
        driver.findElement(By.id("username")).sendKeys("invalid");
        driver.findElement(By.name("password")).sendKeys("Super");
        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+
+       String expectedResult = "Your username is invalid!";
+       String actualResult = driver.findElement(By.id("flash")).getText();
+       System.out.println("actual result: "+ actualResult);
+       Assert.assertTrue(actualResult.contains(expectedResult), "Error Message: text not matching");
 
    }
 
